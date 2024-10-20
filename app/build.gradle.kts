03722eb6ae01d0563fb10066aa06002ab903f12b
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-10-20 17:30:03 +0700

    Fix: It should be =, not - & It should be package, not namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     compileSdk = 34
-    namespace com.nurazlib.autoclicker
+    package com.nurazlib.autoclicker
 
     defaultConfig {
         applicationId = "com.nurazlib.autoclicker"
@@ -29,7 +29,7 @@ android {
 
     buildFeatures {
         viewBinding = true
-        dataBinding - true
+        dataBinding = true
     }
 }
 
